3d0d3c93e46922c409839daf91106c7064e4aeb5
Michał Słomkowski <m.slomkowski@gmail.com>
2015-09-24 17:49:14 +0200

    Add bashrc with color prompt and additional information. #1
    Information includes:
    * non-zero error codes
    * running background jobs
    * running screen sessions

---
+++ bashrc.sh
@@ -0,0 +1,193 @@
+#!/usr/bin/env bash
+
+# ~/.bashrc: executed by bash(1) for non-login shells.
+# see /usr/share/doc/bash/examples/startup-files (in the package bash-doc)
+# for examples
+
+# If not running interactively, don't do anything
+[ -z "$PS1" ] && return
+
+# don't put duplicate lines in the history. See bash(1) for more options
+# ... or force ignoredups and ignorespace
+HISTCONTROL=ignoredups:ignorespace
+
+# append to the history file, don't overwrite it
+shopt -s histappend
+
+# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
+HISTSIZE=200
+HISTFILESIZE=400
+
+# check the window size after each command and, if necessary,
+# update the values of LINES and COLUMNS.
+shopt -s checkwinsize
+
+# make less more friendly for non-text input files, see lesspipe(1)
+[ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
+
+# set variable identifying the chroot you work in (used in the prompt below)
+if [ -z "$debian_chroot" ] && [ -r /etc/debian_chroot ]; then
+    debian_chroot=$(cat /etc/debian_chroot)
+fi
+
+
+# Reset
+Color_Off='\e[0m'       # Text Reset
+
+# Regular Colors
+Black='\e[0;30m'        # Black
+Red='\e[0;31m'          # Red
+Green='\e[0;32m'        # Green
+Yellow='\e[0;33m'       # Yellow
+Blue='\e[0;34m'         # Blue
+Purple='\e[0;35m'       # Purple
+Cyan='\e[0;36m'         # Cyan
+White='\e[0;37m'        # White
+
+# Bold
+BBlack='\e[1;30m'       # Black
+BRed='\e[1;31m'         # Red
+BGreen='\e[1;32m'       # Green
+BYellow='\e[1;33m'      # Yellow
+BBlue='\e[1;34m'        # Blue
+BPurple='\e[1;35m'      # Purple
+BCyan='\e[1;36m'        # Cyan
+BWhite='\e[1;37m'       # White
+
+# Underline
+UBlack='\e[4;30m'       # Black
+URed='\e[4;31m'         # Red
+UGreen='\e[4;32m'       # Green
+UYellow='\e[4;33m'      # Yellow
+UBlue='\e[4;34m'        # Blue
+UPurple='\e[4;35m'      # Purple
+UCyan='\e[4;36m'        # Cyan
+UWhite='\e[4;37m'       # White
+
+# Background
+On_Black='\e[40m'       # Black
+On_Red='\e[41m'         # Red
+On_Green='\e[42m'       # Green
+On_Yellow='\e[43m'      # Yellow
+On_Blue='\e[44m'        # Blue
+On_Purple='\e[45m'      # Purple
+On_Cyan='\e[46m'        # Cyan
+On_White='\e[47m'       # White
+
+# High Intensity
+IBlack='\e[0;90m'       # Black
+IRed='\e[0;91m'         # Red
+IGreen='\e[0;92m'       # Green
+IYellow='\e[0;93m'      # Yellow
+IBlue='\e[0;94m'        # Blue
+IPurple='\e[0;95m'      # Purple
+ICyan='\e[0;96m'        # Cyan
+IWhite='\e[0;97m'       # White
+
+# Bold High Intensity
+BIBlack='\e[1;90m'      # Black
+BIRed='\e[1;91m'        # Red
+BIGreen='\e[1;92m'      # Green
+BIYellow='\e[1;93m'     # Yellow
+BIBlue='\e[1;94m'       # Blue
+BIPurple='\e[1;95m'     # Purple
+BICyan='\e[1;96m'       # Cyan
+BIWhite='\e[1;97m'      # White
+
+# High Intensity backgrounds
+On_IBlack='\e[0;100m'   # Black
+On_IRed='\e[0;101m'     # Red
+On_IGreen='\e[0;102m'   # Green
+On_IYellow='\e[0;103m'  # Yellow
+On_IBlue='\e[0;104m'    # Blue
+On_IPurple='\e[0;105m'  # Purple
+On_ICyan='\e[0;106m'    # Cyan
+On_IWhite='\e[0;107m'   # White
+
+
+if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null
+then
+	# We have color support; assume it's compliant with Ecma-48
+	# (ISO/IEC-6429). (Lack of such support is extremely rare, and such
+	# a case would tend to support setf rather than setaf.)
+	color_prompt=yes
+else
+	color_prompt=
+fi
+
+unset HOST_COLOR
+
+#HOST_COLOR=${UGreen}
+
+function __makePS1() {
+    local EXIT="$?"
+
+    PS1=''
+    PS1+="${debian_chroot:+($debian_chroot)}"
+
+    if [ `id -u` != 0 ]; then
+        PS1+="${Green}\u" # user
+    else
+        PS1+="${Red}\u" # root
+    fi
+
+    PS1+="${Color_Off}@" # @
+
+    if [ ! -n "$HOST_COLOR" ]; then
+        local HOST=$(( (${#HOSTNAME}+0x$(hostid)) % 15+1))
+        HOST_COLOR=$(tput setaf $((host%5 + 2)))
+    fi
+
+    PS1+="${UBlack}${HOST_COLOR}\h${Color_Off}:" # host
+
+    PS1+="${BYellow}\w" # working directory
+
+    # background jobs
+    local NO_JOBS=`jobs -p | wc -w`
+    if [ $NO_JOBS != 0 ]; then
+        PS1+=" ${BIGreen}[j${NO_JOBS}]${Color_Off}"
+    fi
+
+    # screen jobs
+    if [ -d /var/run/screens/S-`whoami` ]; then
+        local SCREEN_JOBS=`ls /var/run/screens/S-\`whoami\` | wc -w`
+        if [ $SCREEN_JOBS != 0 ]; then
+            PS1+=" ${BIGreen}[s${SCREEN_JOBS}]${Color_Off}"
+        fi
+    fi
+
+    # exit code
+    if [ $EXIT != 0 ]; then
+        PS1+=" ${BIRed}[!${EXIT}]${Color_Off}"
+    fi
+
+    PS1+=" ${BPurple}\$${Color_Off} " # prompt
+}
+
+if [ "$color_prompt" = yes ]; then
+    PROMPT_COMMAND=__makePS1
+else
+    PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+fi
+
+unset color_prompt force_color_prompt
+
+# enable color support of ls and also add handy aliases
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    alias ls='ls --color=auto'
+    alias dir='dir --color=auto'
+    alias vdir='vdir --color=auto'
+
+    alias grep='grep --color=auto'
+    alias fgrep='fgrep --color=auto'
+    alias egrep='egrep --color=auto'
+fi
+
+# Alias definitions.
+# You may want to put all your additions into a separate file like
+# ~/.bash_aliases, instead of adding them here directly.
+# See /usr/share/doc/bash-doc/examples in the bash-doc package.
+if [ -f ~/.bash_aliases ]; then
+    . ~/.bash_aliases
+fi
