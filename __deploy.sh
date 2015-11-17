d0b79e534cd2d3ff053bd07849bec6cd365952eb
Michał Słomkowski <m.slomkowski@gmail.com>
2015-11-17 01:40:05 +0100

    Add deploy script.

---
+++ __deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This script is used to deploy the scripts to remote server.
+
+if [ $# -eq 0 ]; then
+     echo -e "Usage:\n$0 user@host"
+     exit 1
+fi
+
+scp bashrc.sh       $1:~/.bashrc
+scp bash_profile.sh $1:~/.bash_profile
+scp bash_aliases.sh $1:~/.bash_aliases
+
