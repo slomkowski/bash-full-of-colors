0152552159f634eb5452c2af9550c8a3f34da90f
Michał Słomkowski <m.slomkowski@gmail.com>
2016-03-08 00:00:26 +0100

    Add status info to git line.

--- bashrc.sh
+++ bashrc.sh
@@ -191,10 +191,18 @@ function __makePS1() {
 
     # git branch
     if [ -x "`which git 2>&1`" ]; then
-        local BRANCH="$(git branch 2>/dev/null | sed -e "/^\s/d" -e "s/^\*\s//")"
+        local branch="$(git name-rev --name-only HEAD 2>/dev/null)"
 
-        if [ -n "${BRANCH}" ]; then
-            PS1+=" \[${BBlue}\](${BRANCH})\[${Color_Off}\]"
+        if [ -n "${branch}" ]; then
+            local git_status="$(git status --porcelain -b 2>/dev/null)"
+            local letters="$( echo "${git_status}" | grep --regexp=' \w ' | sed -e 's/^\s\?\(\w\)\s.*$/\1/' )"
+            local untracked="$( echo "${git_status}" | grep -F '?? ' | sed -e 's/^\?\(\?\)\s.*$/\1/' )"
+            local status_line="$( echo -e "${letters}\n${untracked}" | sort | uniq | tr -d '[:space:]' )"
+            PS1+=" \[${BBlue}\](${branch}"
+            if [ -n "${status_line}" ]; then
+                PS1+=" ${status_line}"
+            fi
+            PS1+=")\[${Color_Off}\]"
         fi
     fi
 
